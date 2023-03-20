75bd46dd2c00b1caeff92683349b62ddb1c7d2ae
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-20 22:49:03 +0000

    fixed some issues with scrolling

--- portfolio-page/src/pages/Portfolio.jsx
+++ portfolio-page/src/pages/Portfolio.jsx
@@ -5,24 +5,26 @@ const Portfolio = () => {
 
   return (
   <>
-    <Parallax pages={7} className='container'>
+    <Parallax pages={1.5} scrollOffset={1000} className='container'>
+        <ParallaxLayer speed={-0.75} factor={0}>
             <img src="../images/6th.png" alt='' className='background'/>
-        <ParallaxLayer speed={-0.5}>
+        </ParallaxLayer>
+        <ParallaxLayer speed={-0.95} factor={1}>
             <img src="../images/Sun.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={0}>
+        <ParallaxLayer speed={-0.65} factor={0}>
             <img src="../images/5th.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={0.5}>
+        <ParallaxLayer speed={-0.5} factor={0.1}>
             <img src="../images/4th.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={1}>
+        <ParallaxLayer speed={0.3} factor={0.1}>
             <img src="../images/3rd.png" alt=''/>
         </ParallaxLayer>        
-        <ParallaxLayer speed={1.25}>
+        <ParallaxLayer speed={0.3} factor={0.1}>
             <img src="../images/2nd.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={1.5}>
+        <ParallaxLayer speed={1.25} factor={0.1}>
             <img src="../images/Bottom.png" alt='' className='bottom'/>
         </ParallaxLayer>
     </Parallax>
