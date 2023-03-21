37bc57303c3adfd3b90f5fef0dc27cf08493f81d
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-21 18:26:28 +0000

    added responsiveness to 320px width

--- portfolio-page/src/pages/Portfolio.jsx
+++ portfolio-page/src/pages/Portfolio.jsx
@@ -4,31 +4,31 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 const Portfolio = () => {
 
   return (
-  <>
-    <Parallax pages={1.3} className='container'>
-        <ParallaxLayer speed={-0.75} factor={0}>
+  <div className='container'>
+    <Parallax pages={1.3} >
+        <ParallaxLayer speed={-0.75} factor={1}>
             <img src="../images/6th.png" alt='' className='background'/>
         </ParallaxLayer>
         <ParallaxLayer speed={-0.95} factor={1}>
             <img src="../images/Sun.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={-0.65} factor={0}>
+        <ParallaxLayer speed={-0.65} factor={1}>
             <img src="../images/5th.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={-0.5} factor={0.1}>
+        <ParallaxLayer speed={-0.65} factor={1}>
             <img src="../images/4th.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={0.3} factor={0.1}>
+        <ParallaxLayer speed={-0.1} factor={1}>
             <img src="../images/3rd.png" alt=''/>
         </ParallaxLayer>        
-        <ParallaxLayer speed={0.3} factor={1}>
+        <ParallaxLayer speed={-0.1} factor={1}>
             <img src="../images/2nd.png" alt=''/>
         </ParallaxLayer>
-        <ParallaxLayer speed={1.25} factor={0.1}>
+        <ParallaxLayer speed={1} factor={1}>
             <img src="../images/Bottom.png" alt='' className='bottom'/>
         </ParallaxLayer>
     </Parallax>
-  </>
+  </div>
   
   )
 }
