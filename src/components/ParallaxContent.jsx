c889d7909911a60e297b016293608e32fd954d12
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-27 21:17:07 +0000

    created component for main content

--- src/components/ParallaxContent.jsx
+++ src/components/ParallaxContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import {useSpring} from '@react-spring/web'
+import Main from './Main'
 
 const ParallaxContent = () => {
 
@@ -47,23 +48,7 @@ const ParallaxContent = () => {
             </div>
           </ParallaxLayer>
           <ParallaxLayer speed={2.5} factor={1}>
-            <main>
-              <div className="content-container">
-                <h1 style={fade} className='name-header'>Hello! My name is Joseph!</h1>
-                <p>
-                  I am a software engineer in Southern California. I'd love to contribute to any projects and applications that have a positive impact on the community.
-                </p>
-                <p>
-                  Before entering the tech industry, I worked in the healthcare field for several years. Over this time, I gained a strong work ethic, with great admiration for the strong teamwork, coordination, and problem-solving that it entailed. I was introduced to the software development world by friends who suggested it due to its similarities to the medical field in the ways that I loved it.
-                </p>
-                <p>
-                  After months of soul-searching, self-studying, and research into the field, I decided to delve into it further and immerse myself in a boot camp program, and I have continued loving it since!
-                </p>
-                <p>
-                  I'm excited to keep moving forward in this industry and solving problems with a like-minded group of teammates and continuing this path of ongoing growth!
-                </p>
-              </div>
-            </main>
+            <Main />
           </ParallaxLayer>
         </Parallax>
   )
