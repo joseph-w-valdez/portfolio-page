4d2eb81ceceabc247ba9a8743380bc0e615724e8
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-29 00:50:18 +0000

    added better mobile responsiveness

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,36 @@
-import React, {useEffect, useState} from 'react'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import ParallaxContent from '../components/ParallaxContent'
+import React, { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import Main from '../components/Main';
+import ParallaxContent from '../components/ParallaxContent';
 
 const Portfolio = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-  const [isMobile, setIsMobile] = useState(false)
-
-  
   useEffect(() => {
     const userAgent = window.navigator.userAgent;
-    setIsMobile(/Mobi/.test(userAgent));
+    const checkIsMobile = () => {
+      setIsMobile(/Mobi/.test(userAgent) || window.innerWidth < 600);
+    };
+
+    // Run once on component mount
+    checkIsMobile();
+
+    // Add event listener for window resizing
+    window.addEventListener('resize', checkIsMobile);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
   }, []);
-  
+
   return (
-    <div className='full-container' >
+    <div className='full-container'>
       {!isMobile ? (
-        <div className='parallax-container' style={{ backgroundColor: '#0d0d0d' }} >
+        <div
+          className='parallax-container'
+          style={{ backgroundColor: '#0d0d0d' }}
+        >
           <ParallaxContent />
         </div>
       ) : (
@@ -24,13 +38,12 @@ const Portfolio = () => {
           <div className='mobile-header'>
             <Header />
           </div>
-          <img src="../images/full.png" alt="" />
+          <img src='../images/full.png' alt='' />
           <Main />
         </div>
-        )
-      }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
