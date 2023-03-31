6bc707da683b21d886b47343b98f08c3fdb75a9c
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-31 19:45:04 +0000

    added react hook form to manage form data.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 const ContactForm = () => {
-  return (
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = async (data) => {
+        try {
+          // send email using your backend code here
+          console.log(data);
+        } catch (error) {
+          // handle 500 error
+          console.error(error);
+        }
+      };
+      
+    return (
     <>
-      <form className='contact-form'>
-        <h2>Send me a message!</h2>
+      <h2>Send me a message!</h2>
+      <div className="flex-basis"></div>
+      <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" id='full-name' placeholder='Full Name' {...register('fullName', { required: true })} />
         <div className="flex-basis"></div>
-        <input type="text" id='full-name' placeholder='Full Name' required/>
+        <input type="email" id='email' placeholder='Email' {...register('email', { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i })} />
         <div className="flex-basis"></div>
-        <input type="email" id='email'placeholder='Email' required/>
+        <input type="text" placeholder='Subject' {...register('subject', { required: true })} />
         <div className="flex-basis"></div>
-        <input type="text" placeholder='Subject' required/>
-        <div className="flex-basis"></div>
-        <textarea id='message' placeholder='Message' required/>
+        <textarea id='message' placeholder='Message' {...register('message', { required: true })} />
         <div className="flex-basis"></div>
         <button>Submit</button>
       </form>
