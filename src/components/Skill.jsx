da97e8cc86940f38383ac7da9e22ad2caabd679d
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-03-31 20:42:31 +0000

    moved a tag to correct order in skill component

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Skill = ({ title, logo, website }) => {
   return (
-    <a href={website} target="_blank" rel="noopener noreferrer">
-      <div className="skill">
+    <div>
+      <a className="skill" href={website} target="_blank" rel="noopener noreferrer">
         <h3>{title}</h3>
         <div className="skill-logo-frame">
           <img src={logo} alt={title} />
         </div>
-      </div>
-    </a>
+      </a>
+    </div>
   );
 };
 
