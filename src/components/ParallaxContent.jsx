f3a507ac27ca9326ceca2be73998cd5a090199d5
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-04-01 01:45:26 +0000

    tweaked parallax settings

--- src/components/ParallaxContent.jsx
+++ src/components/ParallaxContent.jsx
@@ -27,32 +27,32 @@ const ParallaxContent = () => {
 
   return (
     <div>
-      <Parallax ref={parallaxRef} pages={2.03} className='parallax'>
+      <Parallax ref={parallaxRef} pages={3.4} className='parallax'>
         <ParallaxLayer speed={0} factor={5}>
           <img src="../images/6th.png" alt='' className='background'/>
         </ParallaxLayer>
-        <ParallaxLayer speed={-0.2} factor={1}>
+        <ParallaxLayer speed={-0.3} factor={1}>
           <img src="../images/Sun.png" alt=''  />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.2} factor={1}>
+        <ParallaxLayer speed={0.1} factor={1}>
           <img src="../images/5th.png" alt=''  />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.5} factor={1}>
+        <ParallaxLayer speed={0.7} factor={1}>
           <img src="../images/4th.png" alt='' />
         </ParallaxLayer>
-        <ParallaxLayer speed={0.7} factor={1}>
+        <ParallaxLayer speed={1} factor={1}>
           <img src="../images/3rd.png" alt='' />
         </ParallaxLayer>
-        <ParallaxLayer speed={1.1} factor={1}>
+        <ParallaxLayer speed={1.3} factor={1}>
           <img src="../images/2nd.png" alt=''  />
         </ParallaxLayer>
-        <ParallaxLayer speed={1.5} factor={1}>
+        <ParallaxLayer speed={1} factor={1}>
           <img src="../images/Bottom.png" alt='' className='bottom'/>
         </ParallaxLayer>
         <ParallaxLayer speed={-0.3} factor={1}>
           <Header />
         </ParallaxLayer>
-        <ParallaxLayer speed={2.5} factor={1}>
+        <ParallaxLayer speed={0.5} factor={1}>
           <Main />
         </ParallaxLayer>
       </Parallax>
