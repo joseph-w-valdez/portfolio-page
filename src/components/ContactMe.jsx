88754d192d96257086fec9a4efd0b042cbb0b117
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-04-07 23:15:27 +0000

    extracted out socials data into separate data so it may be mapped over

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,44 +1,38 @@
 import React from 'react';
-
 import ContactForm from './ContactForm';
+import { socialsData } from '../data/socialsData';
 
 const ContactMe = () => {
-  return (
-    <div className="section contact-me-section">
-      <h1>Contact Me</h1>
-      <div className="flex-basis"></div>
-      <div className="contact-me-container">
-        <h2>Let's connect! 😊</h2>
+  
+    return (
+      <div className="section contact-me-section">
+        <h1>Contact Me</h1>
         <div className="flex-basis"></div>
-        <div className="socials">
-          <a
-            className="social"
-            href="https://www.linkedin.com/in/joseph-w-valdez/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="social-logo-frame">
-              <img src="../images/LinkedInLogo.png" alt="LinkedIn" />
-            </div>
-            <span className="social-link">linkedin.com/in/joseph-w-valdez</span>
-          </a>
-          <a
-            className="social"
-            href="https://github.com/joseph-w-valdez"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="social-logo-frame">
-              <img src="../images/GitHubLogo.png" alt="GitHub" />
-            </div>
-            <span className="social-link">github.com/joseph-w-valdez</span>
-          </a>
+        <div className="contact-me-container">
+          <h2>Let's connect! 😊</h2>
+          <div className="flex-basis"></div>
+          <div className="socials">
+            {socialsData.map((link, index) => (
+              <a
+                key={index}
+                className="social"
+                href={link.linkUrl}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <div className="social-logo-frame">
+                  <img src={link.logoUrl} alt={link.linkUrl} />
+                </div>
+                <span className="social-link">{link.linkUrl}</span>
+              </a>
+            ))}
+          </div>
+          <div className="flex-basis"></div>
+          <ContactForm />
         </div>
-        <div className="flex-basis"></div>
-        <ContactForm />
       </div>
-    </div>
-  );
-};
-
-export default ContactMe;
+    );
+  };
+  
+  export default ContactMe;
+  
---
+++ src/data/socialsData.js
@@ -0,0 +1,10 @@
+export const socialsData = [
+    {
+      logoUrl: "../images/LinkedInLogo.png",
+      linkUrl: "https://www.linkedin.com/in/joseph-w-valdez/"
+    },
+    {
+      logoUrl: "../images/GitHubLogo.png",
+      linkUrl: "https://github.com/joseph-w-valdez"
+    }
+  ];
