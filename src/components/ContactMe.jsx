51bea03b70a8c829e3305c86171eebc3e4ad1dfe
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-04-07 21:23:21 +0000

    converted social elements to a elements that refer to the corresponding links. adjusted hover styling

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,34 +1,44 @@
-import React from 'react'
+import React from 'react';
 
-import ContactForm from './ContactForm'
+import ContactForm from './ContactForm';
 
 const ContactMe = () => {
   return (
     <div className="section contact-me-section">
-        <h1>Contact Me</h1>
+      <h1>Contact Me</h1>
+      <div className="flex-basis"></div>
+      <div className="contact-me-container">
+        <h2>Let's connect! 😊</h2>
         <div className="flex-basis"></div>
-        <div className="contact-me-container">
-            <h2>Let's connect! 😊</h2>
-            <div className="flex-basis"></div>
-            <div className="socials">
-                <div className="social">
-                    <a className="social-logo-frame" href='https://www.linkedin.com/in/joseph-w-valdez/' target='_blank' rel='noreferrer'>
-                        <img src="../images/LinkedInLogo.png" alt="LinkedIn" />
-                    </a>
-                    <a href='https://www.linkedin.com/in/joseph-w-valdez/' target='_blank' rel='noreferrer' className='social-link'>linkedin.com/in/joseph-w-valdez</a>
-                </div>
-                <div className="social">
-                    <a className="social-logo-frame" href='https://github.com/joseph-w-valdez' target='_blank' rel='noreferrer' >
-                        <img src="../images/GitHubLogo.png" alt="GitHub" />
-                    </a>
-                    <a href='https://github.com/joseph-w-valdez' target='_blank' rel='noreferrer' className='social-link'>github.com/joseph-w-valdez</a>
-                </div>
+        <div className="socials">
+          <a
+            className="social"
+            href="https://www.linkedin.com/in/joseph-w-valdez/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="social-logo-frame">
+              <img src="../images/LinkedInLogo.png" alt="LinkedIn" />
             </div>
-            <div className="flex-basis"></div>
-            <ContactForm />
+            <span className="social-link">linkedin.com/in/joseph-w-valdez</span>
+          </a>
+          <a
+            className="social"
+            href="https://github.com/joseph-w-valdez"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="social-logo-frame">
+              <img src="../images/GitHubLogo.png" alt="GitHub" />
+            </div>
+            <span className="social-link">github.com/joseph-w-valdez</span>
+          </a>
         </div>
+        <div className="flex-basis"></div>
+        <ContactForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactMe
+export default ContactMe;
