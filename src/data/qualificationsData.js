ce3ac76a1019cb22d6c38c0b3f525feefd9c7abd
Joseph Valdez <joseph.w.valdez@gmail.com>
2023-04-07 23:58:22 +0000

    modified qualifications data and map method to render with proper styling

---
+++ src/data/qualificationsData.js
@@ -0,0 +1,37 @@
+export const qualificationsData = [
+    {
+      institution: 'LearningFuze',
+      logoUrl:
+        'https://media.licdn.com/dms/image/C560BAQEimxlasi9TvQ/company-logo_100_100/0/1592771574713?e=1689206400&v=beta&t=9FPoN7kXbnv-CaYANa2HKE-P0x4O7H_Ay3dEaH7zm5A',
+      dates: 'Nov 2022 - Mar 2023',
+      href: 'https://learningfuze.com/',
+      description: [
+        'Full Immersion Program for a career in Software Development in a simulated, agile work environment.',
+        'Over 800 hours of web development programming (70 hours/week), going over critical concepts, including:',
+      ], 
+      bulletpoints: [
+        '• Fundamentals of Web Development (HTML5, CSS3, JavaScript - ES5/6), Flexbox, Git, AJAX, JSON, and APIs',
+        '• Problem-Solving, Debugging, Project Planning in Agile, and Collaboration',
+        '• Implementation of libraries and frameworks (React.js, Node.js, Express, and more)',
+        '• Researching and learning new technologies as needed for application solutions',
+        '• Prototyping and designing with Figma Design Tool'
+      ]
+    },
+    {
+      institution: 'Robert Gardner, Ph.D',
+      role: 'Software Development Instructor',
+      logoUrl:
+        'https://media.licdn.com/dms/image/C4E03AQFCVpKOnAeOUA/profile-displayphoto-shrink_100_100/0/1516167350883?e=1686182400&v=beta&t=UQ0WoZHbV5LnN9u75ChkqLs9oJ82V1PHOUQONGlNLZU',
+      dates: 'March 22, 2023, Robert was Joseph\'s teacher',
+      href: 'https://www.linkedin.com/in/joseph-w-valdez/details/recommendations/',
+      description: [
+        '',
+        'I taught Joseph at LearningFuze, where Joseph learned full stack development with React, Node, and PostgreSQL. This was an intense course that required Joseph to write a full stack application on his own.',
+        'At LearningFuze, Joseph was an exceptional student. He learned quickly and got things done. Joseph was constantly striving to improve his code quality and learn new technologies. For his final project, he wrote an app using the Marvel API. He learned, on his own, an impressive number of technologies for this, such as working with the Marvel API (including the digest technology they use), React Hook Form, React Router, Axios, and Tailwind CSS.',
+        "Joseph was one of my top students, with a great sense of how the computer works, how to write solid, clean code, and how to design and organize the code to make it easy to work with. He found a bug in one of the exercises we've been using for years and helped debug and fix it. Joseph has a good grasp of basic data structures and complexity analysis and how to use them to solve common software problems.",
+        'Joseph has a strong work ethic and is dedicated to getting work done, and done well. He knows when to ask questions and how to understand and make use of the answers to his questions.',
+        'As a former Director of Engineering at Google, I can say with confidence that Joseph will quickly be an asset to any team he joins. He was a joy to teach and I am certain he will have an outstanding career in software development.'
+      ]
+    }
+  ];
+  
