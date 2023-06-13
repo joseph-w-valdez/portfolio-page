513a7bc7bbc3ba2f31b4bf7652933541fc0a7564
Joseph Valdez <Joseph.W.Valdez@gmail.com>
2023-06-13 22:56:07 +0000

    updated css icons in skills data

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -36,7 +36,7 @@ export const skillsData = [
     },
     {
         title: 'CSS3',
-        logo: 'https://github.com/devicons/devicon/raw/master/icons/css3/css3-plain-wordmark.svg',
+        logo: 'https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg',
         website: 'https://developer.mozilla.org/en-US/docs/Web/CSS'
     },
     {
